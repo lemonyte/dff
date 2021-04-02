cd2dc61b4336f44eee657e7fe49b78797197759e
Timothy Pavlushik <49930425+LemonPi314@users.noreply.github.com>
2021-04-02 00:45:42 -0700

    Updated readme.md

--- C#/dff/Program.cs
+++ C#/dff/Program.cs
@@ -6,7 +6,35 @@ namespace dff
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+
+        class FileInfo
+        {
+            
+        }
+
+        /*
+        string[] GetFilePaths(string dir)
+        {
+            string[] filePaths = ["a", "b"];
+            return filePaths;
+        }
+        */
+
+        void MakeDictionary()
+        {
+
+        }
+
+        string GenerateHash(FileInfo fileInfo, string position = "full", int chunkSize = 4096)
+        {
+            return "unhashed";
+        }
+
+        void Log(string dictionary, string dir)
+        {
+
         }
     }
 }
