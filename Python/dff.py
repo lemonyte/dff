31c66121c1ab5fe854341fa330eb9de5d09bac4f
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2021-08-13 05:02:12 -0700

    Add features and fix bugs

--- Python/dff.py
+++ Python/dff.py
@@ -1,185 +1,249 @@
-import os, hashlib, json, ctypes, sys, time
-from pyconsole import *
+"""Duplicate File Finder by LemonPi314
+https://github.com/LemonPi314/dff
+
+Usage:
+  dff [-d <path>] [-o <path>] [-l <level>] [-a] [-xd <pattern>...] [-xf <pattern>...]
+
+Options:
+  -h, --help                                    show this help message and exit
+  -d <path>, --dir <path>                       directory path to search for duplicate files
+  -o <path>, --output-dir <path>                directory path to place output files [default: .]
+  -l <level>, --log <level>                     logging level from 0 to 5 [default: 1]
+  -a, --admin                                   run with administrator privileges
+  -xd <pattern>..., --exclude-dir <pattern>...  exclude directories matching this pattern,
+                                                can be repeated multiple times (e.g. .*)
+  -xf <pattern>..., --exclude-file <pattern>... exclude files matching this pattern,
+                                                can be repeated multiple times (e.g. *.txt)
+
+See https://duplexes.me/pyco/#/logging?id=log-levels for a list of log levels
+See https://docs.python.org/3/library/fnmatch.html for more information on patterns
+
+"""
+
+import os
+import json
+from sys import argv, executable
+from hashlib import md5
+from time import perf_counter
+from fnmatch import fnmatch
+from ctypes import windll
+from pyco import print_message, user_input, logging, progress, prefix, terminal, utils
+
+EMPTY_HASH = 'd41d8cd98f00b204e9800998ecf8427e'
+
 
 class FileInfo:
-    def __init__(self, path):
+    def __init__(self, path: str):
         self.path = path
         self.size = os.path.getsize(self.path)
 
-def GetFilePaths(dir: str):
-    global vaildInput
-    print()
-    PrintMessage("Searching for files...", "Info")
+
+def get_file_paths(dir: str, exclude_dirs: list[str] = [], exclude_files: list[str] = []) -> list[str]:
+    global valid_input
+    print_message()
+    print_message("Searching for files...")
     files = []
-
-    for (dirPath, dirName, fileName) in os.walk(dir):
-        for f in fileName:
-            files.append(os.path.join(dirPath, f))
-
+    dir = os.path.normpath(os.path.expanduser(dir))
+    exclude_dirs = [os.path.normpath(os.path.sep + dir if ':' not in dir else dir) for dir in exclude_dirs]
+    exclude_files = [os.path.normpath(file) for file in exclude_files]
+    for dirpath, dirnames, filenames in os.walk(dir):
+        fulldirs = [os.path.join(dirpath, dir) for dir in dirnames]
+        fullfiles = [os.path.join(dirpath, file) for file in filenames]
+        files.extend(fullfiles)
+        for dirname, fulldir in zip(dirnames.copy(), fulldirs.copy()):
+            for pattern in exclude_dirs:
+                if fnmatch(fulldir, '*' + pattern):
+                    dirnames.remove(dirname)
+                    fulldirs.remove(fulldir)
+                    break
+        for filename, fullfile in zip(filenames.copy(), fullfiles.copy()):
+            for pattern in exclude_files:
+                if fnmatch(filename, pattern):
+                    files.remove(fullfile)
+                    break
     if len(files) == 0:
-        PrintMessage("No files found in the specified directory.", "Error")
-        vaildInput = False
-    
+        print_message("No files found in the specified directory.", prefix.ERROR)
+        valid_input = False
     else:
-        PrintMessage(f"Found {len(files)} files in '{dir}'.", "Info")
+        print_message(f"Found {len(files)} files in '{dir}'.")
         if len(files) == 1:
-            vaildInput = False
-
+            valid_input = False
     return files
 
-def MakeDictionary(dictionary: dict, operation, prefix: str = '', enableProgressBar: bool = True):
-    tempDictionary = {}
-    progressBarTotal = sum(len(val) for val in dictionary.values())
 
-    if progressBarTotal == 0:
+def create_dictionary(dictionary: dict, operation, prefix: str = '', enable_progress_bar: bool = True):
+    temp_dictionary = {}
+    progress_bar_total = sum(len(value) for value in dictionary.values())
+    if progress_bar_total == 0:
         return
-
-    if enableProgressBar == True:
-        progressCounter = 1
-        progressBar = ProgressBar(total=progressBarTotal, prefix=prefix, decimals=3)
-
+    if enable_progress_bar is True:
+        progress_bar = progress.ProgressBar(iteration=1, total=progress_bar_total, prefix=prefix, decimals=3)
     for items in dictionary.values():
         for item in items:
-            tempDictionary.setdefault(operation(item), []).append(item)
-            if enableProgressBar is True:
-                progressBar.suffix = f'...{item.path[-25:]} {progressCounter}/{progressBarTotal}'
-                progressBar.Update(iteration=progressCounter)
-                progressCounter += 1
-
-    for attr in list(tempDictionary):
-        if len(tempDictionary.get(attr)) <= 1:
-            del tempDictionary[attr]
-    
-    if 'unabletohash' in tempDictionary:
-        del tempDictionary['unabletohash']
-
+            temp_dictionary.setdefault(operation(item), []).append(item)
+            if enable_progress_bar is True:
+                progress_bar.increment(suffix=f'{item.path[:12]}...{item.path[-13:]} {progress_bar.iteration}/{progress_bar_total}')
+    for key in temp_dictionary.copy().keys():
+        if len(temp_dictionary.get(key)) <= 1:
+            del temp_dictionary[key]
+    if 'unabletohash' in temp_dictionary:
+        del temp_dictionary['unabletohash']
     dictionary.clear()
-    dictionary.update(tempDictionary)
+    dictionary.update(temp_dictionary)
 
-def GenerateHash(fileInfo: FileInfo, position: str = 'full', chunkSize: int = 4096):
+
+def generate_hash(file_info: FileInfo, position: str = 'full', chunk_size: int = 4096) -> str:
     try:
-        with open(fileInfo.path, 'rb') as file:
-            fileHash = hashlib.md5()
-            size = fileInfo.size
+        with open(file_info.path, 'rb') as file:
+            file_hash = md5()
+            size = file_info.size
             if size < (4096 * 3) and position != 'full':
                 return 'unhashed'
-
             if size > (4096 * 3):
                 if position == 'end':
-                    ending = file.seek(chunkSize, 2)
+                    ending = file.seek(chunk_size, 2)
                     chunk = file.read(ending)
-                    fileHash.update(chunk)
-
+                    file_hash.update(chunk)
                 elif position == 'start':
-                    chunk = file.read(chunkSize)
-                    fileHash.update(chunk)
-
+                    chunk = file.read(chunk_size)
+                    file_hash.update(chunk)
                 elif position == 'middle':
                     middle = file.seek(round(size / 2), 0)
-                    chunk = file.read(middle - (middle - chunkSize))
-                    fileHash.update(chunk)
-                
+                    chunk = file.read(middle - (middle - chunk_size))
+                    file_hash.update(chunk)
             if position == 'full':
-                while chunk := file.read(chunkSize):
-                    fileHash.update(chunk)
-            
-            return fileHash.hexdigest()
-
+                while chunk := file.read(chunk_size):
+                    file_hash.update(chunk)
+            return file_hash.hexdigest()
     except Exception as exception:
-        PrintMessage(f"Could not hash file: {exception}", "Warning")
+        terminal.clear_line()
+        print_message(f"Could not hash file: {exception}", prefix.WARNING)
         return 'unabletohash'
 
-def Log(dictionary: dict, dir: str):
-    outputDir = f'{dir}/dff_output.txt'
+
+def dump(dictionary: dict, dir: str):
+    output_dir = f'{dir}/dff_output.json'
     output = []
     for key, value in dictionary.items():
-        outputObject = {}
-        outputObject['hash'] = key
-        outputObject['size'] = value[0].size
-        outputObject['files'] = []
-        for item in value:
-            outputObject['files'].append(item.path)
-
-        output.append(outputObject)
-    
-    def SortBySize(dictionary):
-        return dictionary['size']
-
-    output.sort(key=SortBySize)
+        output_object = {}
+        output_object['hash'] = key
+        output_object['size'] = value[0].size
+        output_object['files'] = [item.path for item in value]
+        output.append(output_object)
+    output.sort(key=lambda dictionary: dictionary['size'])
     try:
-        outputFile = open(outputDir, 'w')
-        json.dump(output, outputFile, indent=2, separators=('', ': '))
-
+        file = open(output_dir, 'w')
+        json.dump(output, file, indent=2)
     except Exception as exception:
-        PrintMessage(f"Could not create output file. Using alternate location. {exception}", "Warning")
-        dir = projectDir
-        outputDir = f'{dir}/dff_output.txt'
-        outputFile = open(outputDir, 'w')
-        json.dump(output, outputFile, indent=2, separators=('', ': '))
-
+        print_message(f"Could not create output file. Using alternate location. {exception}", prefix.WARNING)
+        dir = os.getcwd()
+        output_dir = f'{dir}/dff_output.txt'
+        file = open(output_dir, 'w')
+        json.dump(output, file, indent=2)
     finally:
-        outputFile.close()
+        file.close()
+    print_message(f"Check the output file in '{dir}' for more information.")
 
-    PrintMessage(f"Check the output file in '{dir}' for more information.", "Info")
 
-def main():
-    global exitProgram
-    global vaildInput
-    vaildInput = True
+def main(dir: str = None, output_dir: str = None, exclude_dirs: list[str] = [], exclude_files: list[str] = []):
+    global exit_program
+    global valid_input
+    valid_input = True
     dictionary = {0: []}
-    userInput = UserInput("Enter a directory to scan: ")
-    startTime = time.time()
-    allFiles = GetFilePaths(userInput)
-    if vaildInput is True:
-        progressCounter = 1
-        progressBarTotal = len(allFiles)
-        progressBar = ProgressBar(prefix="Calculating file sizes...", decimals=3, total=progressBarTotal)
-        for file in allFiles:
+    os.system('mode con cols=150 lines=50')
+    print_message("Duplicate File Finder by LemonPi314", log=False)
+    print_message("https://github.com/LemonPi314/dff", log=False)
+    print_message()
+    dir = os.path.expanduser(os.path.normpath(user_input("Enter a directory to search: ") if dir is None else dir))
+    output_dir = os.path.expanduser(os.path.normpath(dir if output_dir is None else output_dir))
+    logging.log_path = output_dir
+    logging.clear_log()
+    start_time = perf_counter()
+    all_files = get_file_paths(dir, exclude_dirs, exclude_files)
+    if valid_input is True:
+        progress_bar_total = len(all_files)
+        progress_bar = progress.ProgressBar(iteration=1, total=progress_bar_total, prefix="Calculating file sizes", decimals=3)
+        for file in all_files:
             try:
                 dictionary[0].append(FileInfo(file))
-
             except Exception as exception:
-                PrintMessage(f"Could not calculate file size: {exception}", "Warning")
+                terminal.clear_line()
+                print_message(f"Could not calculate file size: {exception}", prefix.WARNING)
+            progress_bar.increment(suffix=f'{file[:12]}...{file[-13:]} {progress_bar.iteration}/{progress_bar_total}')
+        create_dictionary(dictionary, lambda file_info: file_info.size, enable_progress_bar=False)
+        print_message(f"Found {sum(len(value) for value in dictionary.values())} files with matching sizes in '{dir}'.")
+        create_dictionary(dictionary, lambda file_info: generate_hash(file_info, 'middle'), "Hashing files (Phase 1)")
+        files_skipped = len(dictionary.get('unhashed', []))
+        files_matching_minihashes = (sum(len(value) for value in dictionary.values())) - files_skipped
+        if files_matching_minihashes > 0:
+            print_message(f"Found {files_matching_minihashes} files with minihashes that match other files in '{dir}' (Phase 1)")
+        if files_skipped > 0:
+            print_message(f"Skipped minihashing {files_skipped} files because they are smaller than 12 KB.")
+        create_dictionary(dictionary, lambda file_info: generate_hash(file_info, 'full'), "Hashing files (Phase 2)")
+        total_time = perf_counter() - start_time
+        files_found = sum(len(value) for value in dictionary.values())
+        print_message()
+        print_message(f"Found {files_found} duplicate files in '{dir}' in {round(total_time, 6)} seconds.")
+        if sum(len(value) for value in dictionary.values()) != 0:
+            if EMPTY_HASH in dictionary:
+                print_message(f"{len(dictionary.get(EMPTY_HASH))} out of {files_found} files are empty files.")
+            dump(dictionary, output_dir)
+        print_message()
+        print_message("Press any key to exit the program... ")
+        utils.getch()
+        exit_program = True
 
-            progressBar.suffix = f'...{file[-25:]} {progressCounter}/{progressBarTotal}'
-            progressBar.Update(progressCounter)
-            progressCounter += 1
 
-        MakeDictionary(dictionary, lambda fileInfo: fileInfo.size, enableProgressBar=False)
-        PrintMessage(f"Found {sum(len(val) for val in dictionary.values())} files with matching sizes in '{userInput}'.", "Info")
-        MakeDictionary(dictionary, lambda fileInfo: GenerateHash(fileInfo, 'middle'), "Hashing files (Phase 1)...")
-        skippedFiles = len(dictionary.get('unhashed', 0))
-        PrintMessage(f"Found {(sum(len(val) for val in dictionary.values())) - skippedFiles} files with minihashes that match other files in '{userInput}' (Phase 1)", "Info")
-        if 'unhashed' in dictionary:
-            PrintMessage(f"{skippedFiles} files were skipped because they are smaller than 12 KB.", "Info")
-
-        MakeDictionary(dictionary, lambda fileInfo: GenerateHash(fileInfo, 'full'), "Hashing files (Phase 2)...")
-        endTime = time.time()
-        totalTime = endTime - startTime
-        foundFiles = sum(len(val) for val in dictionary.values())
-        print()
-        PrintMessage(f"Found {foundFiles} duplicate files in '{userInput}' in {round(totalTime, 6)} seconds.", "Info")
-        if sum(len(val) for val in dictionary.values()) != 0:
-            if 'd41d8cd98f00b204e9800998ecf8427e' in dictionary:
-                PrintMessage(f"{len(dictionary.get('d41d8cd98f00b204e9800998ecf8427e'))} out of {foundFiles} files are empty files.", "Info")
-            
-            Log(dictionary, userInput)
-
-        UserInput("\nPress enter to exit the program... ")
-        exitProgram = True
-
-if '-a' in sys.argv or '--admin' in sys.argv:
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, sys.argv[0], None, 1)
-
+if '-h' in argv or '--help' in argv:
+    print(__doc__)
+elif ('-a' in argv or '--admin' in argv) and not windll.shell32.IsUserAnAdmin():
+    windll.shell32.ShellExecuteW(None, "runas", executable, ' '.join(argv), None, 1)
 else:
-    exitProgram = False
-    os.system('mode 185, 50')
-    Logger.Set(False, False)
-    PrintMessage("Duplicate File Finder by LemonPi314. https://github.com/LemonPi314/duplicate-file-finder \n")
     try:
-        while exitProgram is False:
-            main()
-
+        dir = None
+        output_dir = None
+        exclude_dirs = []
+        exclude_files = []
+        if '-l' in argv and argv[-1] != '-l':
+            logging.enable_message_logging = logging.enable_input_logging = True
+            try:
+                logging.log_level = int(argv[argv.index('-l') + 1])
+            except ValueError:
+                print_message("Log level value must be of type 'int'.", prefix.ERROR)
+        elif '--log' in argv and argv[-1] != '--log':
+            logging.enable_message_logging = logging.enable_input_logging = True
+            try:
+                logging.log_level = int(argv[argv.index('--log') + 1])
+            except ValueError:
+                print_message("Log level value must be of type 'int'.", prefix.ERROR)
+        if '-d' in argv and argv[-1] != '-d':
+            dir = argv[argv.index('-d') + 1]
+        elif '--dir' in argv and argv[-1] != '--dir':
+            dir = argv[argv.index('--dir') + 1]
+        if '-o' in argv and argv[-1] != '-o':
+            output_dir = argv[argv.index('-o') + 1]
+        elif '--output-dir' in argv and argv[-1] != '--output-dir':
+            output_dir = argv[argv.index('--output-dir') + 1]
+        if '-xd' in argv:
+            indices = [index for index, element in enumerate(argv) if element == '-xd']
+            exclude_dirs.extend([argv[index + 1] for index in indices if index != len(argv) - 1])
+        if '--exclude-dir' in argv:
+            indices = [index for index, element in enumerate(argv) if element == '--exclude-dir']
+            exclude_dirs.extend([argv[index + 1] for index in indices if index != len(argv) - 1])
+        if '-xf' in argv:
+            indices = [index for index, element in enumerate(argv) if element == '-xf']
+            exclude_files.extend([argv[index + 1] for index in indices if index != len(argv) - 1])
+        if '--exclude-file' in argv:
+            indices = [index for index, element in enumerate(argv) if element == '--exclude-file']
+            exclude_files.extend([argv[index + 1] for index in indices if index != len(argv) - 1])
+        exit_program = False
+        while exit_program is False:
+            main(dir, output_dir, exclude_dirs, exclude_files)
+    except KeyboardInterrupt:
+        print_message("\nKeyboard interrupt detected. Shutting down.")
+        print_message("\nPress any key to exit the program...")
+        utils.getch()
     except Exception as exception:
-        PrintMessage(f"Fatal error: {exception}", "Error")
-        input("\nPress enter to exit the program...")
+        print_message(exception, prefix.ERROR)
+        print_message("\nPress any key to exit the program...")
+        utils.getch()
--- readme.md
+++ readme.md
@@ -1,33 +1,88 @@
 # Duplicate File Finder
 By [LemonPi314](https://github.com/LemonPi314)
 
-A simple console program that finds duplicate files on your PC.
+A simple command line tool to find duplicate files on your PC.
+## Overview
+## Overview
+- [Requirements](#requirements)
+    - [Python File](#python-file)
+    - [Executable File](#executable-file)
+- [Usage](#usage)
+    - [Command Line](#command-line)
+- [Versions](#versions)
+- [How It Works](#how-it-works)
+    - [Size](#size)
+    - [Minihash](#minihash)
+    - [Full Hash](#full-hash)
+- [Disclaimer](#disclaimer)
+- [License](#license)
 ## Requirements
-* Windows operating system
-* 20 MB of free space for the executable
-* 50 MB of free space for temporary files
+### Python File
+- Windows operating system
+- [Python 3.9](https://www.python.org/downloads/) or higher
+- [`pyco`](https://test.pypi.org/project/pyco)
+### Executable File
+Optional executable file. Python and any required packages are included in the executable.
+- 7 MB of free space for the executable
+- 6 MB of free space for temporary files
 ## Usage
-Download the project files and extract the `dff.exe` file from `duplicate-file-finder/Python/dist`. Run the `dff.exe` file and enter a directory you would like to scan for duplicate files. It is recommended to scan only user-specific files such as your `Documents` folder, as scanning your entire disk including the Windows operating system will take a long time and produce a very large output file filled with system file names. After the program is finished it will create an output file named `dff_output.txt` in the directory you specified. The output file is formatted as a list of dictionaries, with each dictionary containing a list of files matching a hash and size.  
-Note: This program does not automatically delete any files. It is up to the user to delete or modify files.
-## Variants
-This program has 3 variants:
-* Python
-* Java (in progress)
-* C# (in progress)
+> Note: these instructions refer to the Python version of the tool.
 
-All 3 variants do the same thing, but were made using different programming languages.
+Download the project files and extract `dff.exe` from `dff/Python/dist`. Double click `dff.exe` and enter a directory path to search for duplicate files.
+### Command Line
+For advanced options, you may run `dff.exe` from a command line terminal such as Command Prompt or PowerShell. Navigate to the same directory as the tool and run the following command with any necessary options.
+
+Command syntax:
+```bash
+dff [-d <path>] [-o <path>] [-l <level>] [-a] [-xd <pattern>...] [-xf <pattern>...]
+```
+Some terminals such as PowerShell do not automatically load files from the current directory, in which case you must replace `dff` with `./dff`. This tool works best in Command Prompt. If you are using the Python file, replace `dff` with `python dff.py`.
+
+Options:
+|Option|Description|
+|--|--|
+|`-h`, `--help`|Show this help message and exit.|
+|`-d <path>`, `--dir <path>`|Directory path to search.|
+|`-o <path>`, `--output-dir <path>`|Directory path to place the output file and log files. Default is the current working directory.|
+|`-l <level>`, `--log <level>`|Log level integer from 0 to 5. Default is 1.|
+|`-a`, `--admin`|Run with administrator privileges.|
+|`-xd <pattern>...`, `--exclude-dir <pattern>...`|Exclude directories matching this pattern, can be repeated multiple times. (e.g. `.*`)|
+|`-xf <pattern>...`, `--exclude-file <pattern>...`|Exclude files matching this pattern, can be repeated multiple times. (e.g. `*.txt`)|
+
+Example:
+```bash
+dff -a -d ~ -o ~/Desktop -xd .* -xd AppData
+```
+Exclude pattern examples:
+- Directories:
+    - `.*`
+    - `AppData`
+    - `Unity`
+    - `build`
+- Files:
+    - `license.txt`
+    - `readme.md`
+    
+
+In all path options (`-d` and `-o`) the `~` character resolves to the current user's home directory.  
+See the [`pyco` docs](https://duplexes.me/pyco/#/logging?id=log-levels) for a list of log levels.  
+See the [Python `fnmatch` docs](https://docs.python.org/3/library/fnmatch.html) for more information on patterns.
+
+It is recommended to search only user-specific files such as your `Documents` folder, as searching the entire disk including the Windows operating system will take exceedingly long and produce a very large output file filled with system file names. After the tool is finished it will create an output file named `dff_output.json` in the directory you specified. The output file is formatted as a list of JSON objects (or Python dictionaries) sorted by file size, with each object containing a list of files matching a hash and size. This tool does not automatically delete any files. It is up to the user to delete or modify files.
+## Versions
+This tool has 3 versions written in Python, Java (in progress), and C# (in progress). All 3 versions do the same thing, but were made using different programming languages.
 ## How It Works
-The program has 3 stages of filtering:  
-1. Sort files by size
-2. Sort files by minihash
-3. Sort files by full hash
+The tool has 3 stages of filtering:  
+1. Filter files by size
+2. Filter files by minihash
+3. Filter files by full hash
 ### Size
-Self explanatory. Files with matching sizes (in bytes) move onto the next stage.
+A file size in bytes is calculated for every file. Files with matching sizes continue onto the next stage.
 ### Minihash
-A "minihash" is calculated for files with matching sizes. A "minihash" is an `MD5` checksum hash of the middle 4 kilobytes of the file. Calculating a minihash before a full hash saves time by eliminating different files with matching sizes.
+A "minihash" is calculated for every file. A "minihash" is an MD5 hash of the middle 4 kilobytes of the file. Calculating a minihash before a full hash saves time by eliminating different files with matching sizes. Files with matching minihashes continue onto the next stage. Files less than 12 kilobytes are skipped and continue onto the next stage.
 ### Full Hash
-A full `MD5` checksum hash is calculated for files with matching minihashes. Files that have matching hashes are duplicates (however there is a very small chance that the hash matches for different files).
+A full MD5 hash is calculated for every file. Files that have matching hashes are duplicates. The tool also lists empty files. It may be worth noting that there is an extremely small chance of two MD5 hashes matching for files that are different. MD5 collisions do not affect this tool significantly, the only consequence is the output will list two different files as duplicates, which the user will be able to notice and take into account.
 ## Disclaimer
-I am not liable for any data loss, damage, or any other consequences due to corruption, improper encryption/decryption, or general use of this software. Security is not guaranteed. Use at your own risk.
+I am not liable for any data loss, damage, or any other consequences resulting from corruption or general use of this software. Security is not guaranteed. Use at your own risk.
 ## License
 [MIT License](https://choosealicense.com/licenses/mit/)
