9a43238ae7412012debf1bbc7efaddea854af937
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-10-15 23:10:49 -0700

    Update Python impl to match Rust impl

--- src/dff/dff.py
+++ src/dff/dff.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import fnmatch
 import functools
 import hashlib
@@ -6,16 +8,14 @@ import os
 import re
 import sys
 import time
-from enum import Enum
+from dataclasses import dataclass
+from enum import StrEnum
 from pathlib import Path
 from typing import Annotated, Optional
 
 from rich.console import Console
 from rich.progress import MofNCompleteColumn, Progress
 from typer import Option, Typer
-
-app = Typer(invoke_without_command=True, no_args_is_help=True)
-entrypoint = functools.partial(app, windows_expand_args=False)
 
 EMPTY_HASH = bytes.fromhex("d41d8cd98f00b204e9800998ecf8427e")
 
@@ -24,10 +24,31 @@ MATCH_SEP_OR_END = r"(?:/|\\|\Z)"
 MATCH_NON_RECURSIVE = r"[^/\\]*"
 MATCH_RECURSIVE = r"(?:.*)"
 
+app = Typer(invoke_without_command=True, no_args_is_help=True)
+entrypoint = functools.partial(app, windows_expand_args=False)
 
-class HashType(Enum):
+
+@dataclass
+class FileInfo:
+    path: Path
+    size: int
+    hash: bytes | None
+
+
+class HashType(StrEnum):
     FULL = "full"
     PARTIAL = "partial"
+
+
+class CompareMethod(StrEnum):
+    SIZE = "size"
+    PARTIAL_HASH = "partial-hash"
+    HASH = "hash"
+
+
+class OutputFormat(StrEnum):
+    JSON = "json"
+    LIST = "list"
 
 
 def glob_to_re(pattern: str) -> str:
@@ -63,13 +84,14 @@ def match_glob(path: Path, pattern: str) -> bool:
 
     If the pattern ends with a directory separator, the path must be a directory.
     """
-    match = bool(re.fullmatch(glob_to_re(pattern), str(path)))
+    flags = re.IGNORECASE if os.name == "nt" else 0
+    match = bool(re.fullmatch(glob_to_re(pattern), str(path), flags=flags))
     if pattern.endswith(("/", "\\")):
         return match and path.is_dir()
     return match
 
 
-def get_file_paths(dirs: list[Path], exclude: list[str]) -> list[Path]:
+def get_files(dirs: list[Path], exclude: list[str]) -> list[FileInfo]:
     files = []
     for dir in dirs:
         for dirname, _, filenames in os.walk(dir):
@@ -77,22 +99,22 @@ def get_file_paths(dirs: list[Path], exclude: list[str]) -> list[Path]:
                 path = Path(dirname, filename)
                 if any(match_glob(path, pattern) for pattern in exclude):
                     continue
-                files.append(path)
+                files.append(FileInfo(path=path, size=path.stat().st_size, hash=None))
     return files
 
 
-def hash_file(path: Path, hash_type: HashType = HashType.FULL, chunk_size: int = 64 * 1024) -> bytes:
-    size = path.stat().st_size
-    if size == 0:
+def hash_file(file_info: FileInfo, hash_type: HashType = HashType.FULL, chunk_size: int = 64 * 1024) -> bytes:
+    if file_info.size == 0:
         return EMPTY_HASH
-    # TODO: return already calculated hash when file info struct is implemented.
-    hasher = hashlib.md5()  # noqa: S324
-    with path.open("rb") as file:
-        if hash_type is HashType.FULL or size <= chunk_size * 3:
+    if file_info.hash and file_info.size < chunk_size * 3:
+        return file_info.hash
+    hasher = hashlib.md5(usedforsecurity=False)
+    with file_info.path.open("rb") as file:
+        if hash_type is HashType.FULL or file_info.size <= chunk_size * 3:
             hasher.update(file.read())
         elif hash_type is HashType.PARTIAL:
             hasher.update(file.read(chunk_size))
-            file.seek((size // 2) - (chunk_size // 2), os.SEEK_SET)
+            file.seek((file_info.size // 2) - (chunk_size // 2), os.SEEK_SET)
             hasher.update(file.read(chunk_size))
             file.seek(-chunk_size, os.SEEK_END)
             hasher.update(file.read(chunk_size))
@@ -104,75 +126,81 @@ def main(
     *,
     dirs: list[Path],  # TODO: fix other options taken as paths
     exclude: Annotated[
-        Optional[list[str]],  # noqa: FA100, Typer does not support "Type | None" syntax
+        Optional[list[str]],  # noqa: UP007; Typer does not support "Type | None" syntax
         Option(help="Exclude files matching this glob pattern."),
     ] = None,
+    compare_method: CompareMethod = CompareMethod.HASH,
+    output_format: OutputFormat = OutputFormat.JSON,
     fail_on_duplicate: Annotated[bool, Option(help="Exit with error code 1 if duplicates are found.")] = False,
 ) -> None:
-    exclude = exclude or []
-    console = Console(stderr=True)
     dirs = [path.expanduser() for path in dirs]
     dirs = [path for path in dirs if path.exists() and path.is_dir()]
+    exclude = exclude or []
+    # Typer does not support IntEnums, so we need to convert manually.
+    compare_methods = list(CompareMethod)
+    compare_method_index = compare_methods.index(compare_method)
+    console = Console(stderr=True)
     start_time = time.perf_counter()
+    sorted: dict[int | bytes, list[FileInfo]] = {}
 
     with Progress(*Progress.get_default_columns(), MofNCompleteColumn(), console=console) as progress:
         task = progress.add_task("Searching for files", total=None)
-        all_files = get_file_paths(dirs, exclude)
-        progress.update(task, total=len(all_files), completed=len(all_files))
-    console.print(f"Found {len(all_files)} files.")
+        files = get_files(dirs, exclude)
+        progress.update(task, total=len(files), completed=len(files))
+    total = len(files)
+    console.print(f"Found {total} files.")
 
-    sizes: dict[int, list[Path]] = {}
-    with Progress(*Progress.get_default_columns(), MofNCompleteColumn(), console=console) as progress:
-        task = progress.add_task("Checking file sizes", total=len(all_files))
-        for file in all_files:
-            sizes.setdefault(file.stat().st_size, []).append(file)
-            progress.advance(task)
-    files = [item for items in sizes.values() if len(items) > 1 for item in items]
+    for file in files:
+        sorted.setdefault(file.size, []).append(file)
+    files = [item for items in sorted.values() if len(items) > 1 for item in items]
     console.print(f"Found {len(files)} files with matching sizes.")
 
-    minihashes: dict[bytes, list[Path]] = {}
-    if files:
-        with Progress(*Progress.get_default_columns(), MofNCompleteColumn(), console=console) as progress:
-            task = progress.add_task("Minihashing files", total=len(files))
-            for file in files:
-                minihashes.setdefault(hash_file(file, HashType.PARTIAL), []).append(file)
-                progress.advance(task)
-        files = [item for items in minihashes.values() if len(items) > 1 for item in items]
-        skipped = len(minihashes.get(b"", []))
-        matching_minihash = len(files) - skipped
-        if matching_minihash:
-            console.print(f"Found {matching_minihash} files with minihashes that match other files.")
-        if skipped:
-            console.print(f"Skipped minihashing {skipped} files because they are too small.")
+    if compare_method_index >= compare_methods.index(CompareMethod.PARTIAL_HASH):
+        sorted.clear()
+        if files:
+            with Progress(*Progress.get_default_columns(), MofNCompleteColumn(), console=console) as progress:
+                task = progress.add_task("Calculating partial hashes", total=len(files))
+                for file in files:
+                    hash = hash_file(file, HashType.PARTIAL)
+                    file.hash = hash
+                    sorted.setdefault(file.hash, []).append(file)
+                    progress.advance(task)
+            files = [item for items in sorted.values() if len(items) > 1 for item in items]
+            console.print(f"Found {len(files)} files with partial hashes that match other files.")
 
-    hashes: dict[bytes, list[Path]] = {}
-    if files:
-        with Progress(*Progress.get_default_columns(), MofNCompleteColumn(), console=console) as progress:
-            task = progress.add_task("Hashing files", total=len(files))
-            for file in files:
-                hashes.setdefault(hash_file(file, HashType.FULL), []).append(file)
-                progress.advance(task)
-        files = [item for items in hashes.values() if len(items) > 1 for item in items]
-    duplicate = len(files)
+    if compare_method_index >= compare_methods.index(CompareMethod.HASH):
+        sorted.clear()
+        if files:
+            with Progress(*Progress.get_default_columns(), MofNCompleteColumn(), console=console) as progress:
+                task = progress.add_task("Calculating full hashes", total=len(files))
+                for file in files:
+                    hash = hash_file(file, HashType.FULL)
+                    file.hash = hash
+                    sorted.setdefault(file.hash, []).append(file)
+                    progress.advance(task)
+            files = [item for items in sorted.values() if len(items) > 1 for item in items]
+            console.print(f"Found {len(files)} files with hashes that match other files.")
 
     total_time = time.perf_counter() - start_time
-    if duplicate:
-        console.print(
-            f"Found {duplicate} duplicate files in {total_time:.4f} seconds"
-            f" ({duplicate / len(all_files):.2%} of all files).",
-        )
-    if EMPTY_HASH in hashes:
-        console.print(f"{len(hashes.get(EMPTY_HASH, ()))} out of {duplicate} files are empty files.")
-
+    console.print(
+        f"Found {len(files)} duplicate files in {total_time:.4f} seconds"
+        f" ({len(files) / total:.2%} of all files).",
+    )
     output = [
         {
-            "hash": hash.hex(),
-            "size": paths[0].stat().st_size,
-            "files": [str(item) for item in paths],
+            "hash": key.hex() if isinstance(key, bytes) else None,
+            "size": files[0].size,
+            "paths": [str(file.path) for file in files],
         }
-        for hash, paths in hashes.items()
-        if len(paths) > 1
+        for key, files in sorted.items()
+        if len(files) > 1
     ]
     output.sort(key=lambda item: item["size"])
-    json.dump(output, sys.stdout, indent=2)
+    if output_format is OutputFormat.JSON:
+        json.dump(output, sys.stdout, indent=2)
+    elif output_format is OutputFormat.LIST:
+        for item in output:
+            print("\n", item["hash"] if item["hash"] else item["size"])
+            for path in item["paths"]:
+                print(f"  {path}")
     raise SystemExit(int(fail_on_duplicate))
